23c01b2634a0878ae152e48a4dd2b2860f462301
pchatz06 <pchatz06@ucy.ac.cy>
2025-07-10 15:27:55 +0300

    minor changes

--- pchatz06_work/SEARCH/simulation.sh
+++ pchatz06_work/SEARCH/simulation.sh
@@ -1,23 +1,31 @@
 trap "echo 'Terminating all child processes'; kill -- -$$" SIGINT SIGTERM EXIT
 
-./run.sh local Blender M10 Onepoint &
-./run.sh local Bwaves M10 Onepoint &
-./run.sh local Cam4 M10 Onepoint &
-./run.sh local cactuBSSN M10 Onepoint &
-./run.sh local Exchange M10 Onepoint &
-./run.sh local Gcc M10 Onepoint &
-./run.sh local Lbm M10 Onepoint &
-./run.sh local Mcf M10 Onepoint &
-./run.sh local Parest M10 Onepoint &
-./run.sh local Povray M10 Onepoint &
-./run.sh local Wrf M10 Onepoint &
-./run.sh local Xalancbmk M10 Onepoint &
-./run.sh local Fotonik3d M10 Onepoint &
-./run.sh local Imagick M10 Onepoint &
-./run.sh local Leela M10 Onepoint &
-./run.sh local Omnetpp M10 Onepoint &
-./run.sh local Perlbench M10 Onepoint &
-./run.sh local Roms M10 Onepoint &
-./run.sh local x264 M10 Onepoint &
-./run.sh local Xz M10 Onepoint &
-./run.sh global Blender:Bwaves:Cam4:cactuBSSN:Exchange:Gcc:Lbm:Mcf:Parest:Povray:Wrf:Xalancbmk:Fotonik3d:Imagick:Leela:Omnetpp:Perlbench:Roms:x264:Xz M10 Onepoint
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <mutation_rate: (M5, M10, etc)> <crossover (Onepoint/Uniform)>"
+    exit 1
+fi
+
+mutation_rate="$1"       # 'local' or 'global'
+crossover="$2"
+
+./run.sh local Blender "$1" "$2" &
+./run.sh local Bwaves "$1" "$2" &
+./run.sh local Cam4 "$1" "$2" &
+./run.sh local cactuBSSN "$1" "$2" &
+./run.sh local Exchange "$1" "$2" &
+./run.sh local Gcc "$1" "$2" &
+./run.sh local Lbm "$1" "$2" &
+./run.sh local Mcf "$1" "$2" &
+./run.sh local Parest "$1" "$2" &
+./run.sh local Povray "$1" "$2" &
+./run.sh local Wrf "$1" "$2" &
+./run.sh local Xalancbmk "$1" "$2" &
+./run.sh local Fotonik3d "$1" "$2" &
+./run.sh local Imagick "$1" "$2" &
+./run.sh local Leela "$1" "$2" &
+./run.sh local Omnetpp "$1" "$2" &
+./run.sh local Perlbench "$1" "$2" &
+./run.sh local Roms "$1" "$2" &
+./run.sh local x264 "$1" "$2" &
+./run.sh local Xz "$1" "$2" &
+./run.sh global Blender:Bwaves:Cam4:cactuBSSN:Exchange:Gcc:Lbm:Mcf:Parest:Povray:Wrf:Xalancbmk:Fotonik3d:Imagick:Leela:Omnetpp:Perlbench:Roms:x264:Xz "$1" "$2"
